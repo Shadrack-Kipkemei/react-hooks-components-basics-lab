0d67a22a77ae819a50ff4998d916f99d40c30572
sktanui <shadrackkemei2019@gmail.com>
2024-10-29 15:09:48 +0300

    complete lab

--- src/components/App.js
+++ src/components/App.js
@@ -17,13 +17,22 @@ function Home() {
 }
 
 {/* write an <About> component here */}
+function About() {
+  return (
+    <div id="about">
+      <h1>About</h1>
+    </div>
+  );
+}
 
 
 function App() {
   return (
     <div>
       <NavBar />
+      <Home/>
       {/* add the <Home> component here */}
+      <About/>
       {/* add your <About> component here */}
     </div>
   );
